bf47f4a54666f680a21ef7b80adc5b6d5f1c4f29
Maria <makarpikova@gmail.com>
2024-06-25 16:02:18 +0800

    HTML, CSS

--- src/main/java/com/example/sneakysearch/SearchController.java
+++ src/main/java/com/example/sneakysearch/SearchController.java
@@ -3,7 +3,6 @@ package com.example.sneakysearch;
 import com.example.sneakysearch.excel.HeadersMy;
 import com.example.sneakysearch.excel.WrittenToExcel;
 import com.example.sneakysearch.excel.WrittenToFile;
-import com.example.sneakysearch.text.FirstWord;
 import org.cactoos.text.Lowered;
 import org.cactoos.text.TextOf;
 import org.springframework.stereotype.Controller;
@@ -14,11 +13,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class SearchController {
     @RequestMapping(value = "/search", method = RequestMethod.GET)
-    public String doSearch(@RequestParam(value = "word") String word) {
+    public String doSearch(@RequestParam(value = "word") String word, @RequestParam(value = "checkbox") boolean cb) {
         new Thread(() -> {
             try {
                 final WrittenToFile writtenToFile = new WrittenToExcel(
-                        new FirstWord(new Lowered(new TextOf(word))).asString(),
+                        (new Lowered(new TextOf(word))).asString(),
                         new HeadersMy());
                 writtenToFile.writeToFile();
             } catch (Exception e) {
